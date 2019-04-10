de2c381ba9c3aca8c3697fd7964e71f9bc34d030
tuananh.le <tuananh.le@foodycorp.vn>
2019-04-10 21:45:42 +0700

    update rule

--- baitap1.py
+++ baitap1.py
@@ -6,9 +6,11 @@ import sys
 from antlr4.tree.Tree import TerminalNodeImpl, ErrorNodeImpl
 from antlr4.error.ErrorListener import ErrorListener
 from antlr4.error.Errors import RecognitionException, NoViableAltException, InputMismatchException, FailedPredicateException, ParseCancellationException
-    
-filterList = ['IDENTIFIER','INTLITERAL','CHARLITERAL','STRINGLITERAL','BOOLEANLITERAL']
+
+filterList = ['IDENTIFIER', 'INTLITERAL',
+              'CHARLITERAL', 'STRINGLITERAL', 'BOOLEANLITERAL']
 fWrite = open(sys.argv[2] + '.out', 'w')
+
 
 def printOutChildNode(child, lexers):
     global filterList, fWrite
@@ -19,6 +21,7 @@ def printOutChildNode(child, lexers):
         fWrite.write('{0} {1} {2}\n'.format(line, token, lexer))
     else:
         fWrite.write('{0} {1}\n'.format(line, lexer))
+
 
 def flattenTree(parent, lexers):
     if (int(sys.argv[1]) == 1):
@@ -31,70 +34,67 @@ def flattenTree(parent, lexers):
             else:
                 flattenTree(child, lexers)
 
-class MyErrorListener( ErrorListener ):
+
+class MyErrorListener(ErrorListener):
     def __init__(self):
         super(MyErrorListener, self).__init__()
-    
-    def printSyntaxError(self, msg, line, column):
-        fWrite.write('[Syntax Error] Line {0}, column {1}: {2}\n'.format(line, column, msg))
 
-    def escapeWSAndQuote(self, s:str):
-        s = s.replace("\n","\\n")
-        s = s.replace("\r","\\r")
-        s = s.replace("\t","\\t")
+    def printSyntaxError(self, msg, line, column):
+        fWrite.write(
+            '[Syntax Error] Line {0}, column {1}: {2}\n'.format(line, column, msg))
+
+    def escapeWSAndQuote(self, s: str):
+        s = s.replace("\n", "\\n")
+        s = s.replace("\r", "\\r")
+        s = s.replace("\t", "\\t")
         return "'" + s + "'"
 
-    def getTokenErrorDisplay(self, t:Token):
+    def getTokenErrorDisplay(self, t: Token):
         if t is None:
             return "<no token>"
         s = t.text
         if s is None:
-            if t.type==Token.EOF:
+            if t.type == Token.EOF:
                 s = "<EOF>"
             else:
                 s = "<" + str(t.type) + ">"
         return self.escapeWSAndQuote(s)
-    
-    def reportNoViableAlternative(self, recognizer:Parser, e:NoViableAltException, line, column):
+
+    def reportNoViableAlternative(self, recognizer: Parser, e: NoViableAltException, line, column):
         tokens = recognizer.getTokenStream()
         if tokens is not None:
-            if e.startToken.type==Token.EOF:
+            if e.startToken.type == Token.EOF:
                 input = "<EOF>"
             else:
                 input = tokens.getText()
         else:
             input = "<unknown input>"
         if (input.strip() != ''):
-            msg = 'No viable alternative at input {0}'.format(self.escapeWSAndQuote(input))
+            msg = 'No viable alternative at input {0}'.format(
+                self.escapeWSAndQuote(input))
             self.printSyntaxError(msg, line, column)
 
-    def reportInputMismatch(self, recognizer:Parser, e:InputMismatchException, line, column):
-        msg = "Mismatched input {0}. Expected {1}".format(self.getTokenErrorDisplay(e.offendingToken), e.getExpectedTokens().toString(recognizer.literalNames, recognizer.symbolicNames))
+    def reportInputMismatch(self, recognizer: Parser, e: InputMismatchException, line, column):
+        msg = "Mismatched input {0}. Expected {1}".format(self.getTokenErrorDisplay(
+            e.offendingToken), e.getExpectedTokens().toString(recognizer.literalNames, recognizer.symbolicNames))
         self.printSyntaxError(msg, line, column)
-        
+
     def reportFailedPredicate(self, recognizer, e, line, column):
         ruleName = recognizer.ruleNames[recognizer._ctx.getRuleIndex()]
-        msg = "Following Rule is error: {0}. Message: {1}".format(ruleName, e.message)
+        msg = "Following Rule is error: {0}. Message: {1}".format(
+            ruleName, e.message)
         self.printSyntaxError(msg, line, column)
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        if isinstance( e, NoViableAltException ):
+        if isinstance(e, NoViableAltException):
             self.reportNoViableAlternative(recognizer, e, line, column)
-        elif isinstance( e, InputMismatchException ):
+        elif isinstance(e, InputMismatchException):
             self.reportInputMismatch(recognizer, e, line, column)
-        elif isinstance( e, FailedPredicateException ):
+        elif isinstance(e, FailedPredicateException):
             self.reportFailedPredicate(recognizer, e, line, column)
         else:
             self.printSyntaxError(msg, line, column)
 
-    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
-        pass
-
-    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-        pass
-        
-    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-        pass
 
 def main(argv):
     input_stream = FileStream(argv[2])
@@ -102,13 +102,14 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = SimpleCodeParser(stream)
     if (int(sys.argv[1]) == 1):
-        parser._listeners = [ MyErrorListener() ]
+        parser._listeners = [MyErrorListener()]
     tree = parser.program()
     flattenTree(tree, lexer)
 
     # printer = SimpleCodePrintListener(lexer)
     # walker = ParseTreeWalker()
     # walker.walk(printer, tree)
+
 
 if __name__ == '__main__':
     main(sys.argv)
